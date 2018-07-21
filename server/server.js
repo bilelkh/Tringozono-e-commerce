3eaa0078ec86c920260dc3293909cd1cb97683d0
William <william.cruz6@fatec.sp.gov.br>
2018-07-21 13:34:59 -0300

    new route

--- server/server.js
+++ server/server.js
@@ -23,7 +23,12 @@ app.use(morgan('dev'));
 app.use(cors());
 
 const userRoutes = require('./routes/account');
+const mainRoutes = require('./routes/main');
+const sellerRoutes = require('./routes/seller');
+
+app.use('/api', mainRoutes);
 app.use('/api/accounts', userRoutes);
+app.use('/api/seller', sellerRoutes);
 
 app.listen(3030, (err) => {
     console.log("Port: " + config.port);
