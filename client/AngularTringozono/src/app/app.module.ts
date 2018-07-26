bd589cbe02897c7603824f6cdc57041fab28764c
William <william.cruz6@fatec.sp.gov.br>
2018-07-26 19:14:45 -0300

    new routes

--- client/AngularTringozono/src/app/app.module.ts
+++ client/AngularTringozono/src/app/app.module.ts
@@ -9,17 +9,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { RestApiService } from './rest-api.service';
+import { DataService } from './data.service';
+import { AuthGuardService } from './auth-guard.service';
+
 import { HomeComponent } from './home/home.component';
 import { MessageComponent } from './message/message.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
-
-import { RestApiService } from './rest-api.service';
-import { DataService } from './data.service';
-import { AuthGuardService } from './auth-guard.service';
 import { ProfileComponent } from './profile/profile.component';
 import { SettingsComponent } from './settings/settings.component';
 import { AddressComponent } from './address/address.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { PostProductComponent } from './post-product/post-product.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +33,8 @@ import { AddressComponent } from './address/address.component';
     ProfileComponent,
     SettingsComponent,
     AddressComponent,
+    CategoriesComponent,
+    PostProductComponent,
   ],
   imports: [
     BrowserModule,
